b9c773518c69c176a948c98db680150f0ff78ede
Kile-Butler01 <gucigovislam39@gmail.com>
2026-02-17 17:45:35 +0300

    More fixes

--- webstore/webstore/settings.py
+++ webstore/webstore/settings.py
@@ -36,7 +36,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '.onrender.com',
     'localhost',
-    '127.0.1',    
+    '127.0.0.1',    
     ]
 
 
@@ -57,15 +57,17 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'webstore.urls'
 
