fc0777cf5a26127137fc21dd4a5cc0b10c155fe4
Kile-Butler01 <gucigovislam39@gmail.com>
2026-02-17 17:30:06 +0300

    Small fixes for Render

--- webstore/webstore/settings.py
+++ webstore/webstore/settings.py
@@ -33,7 +33,11 @@ SECRET_KEY=os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['CoffeApp.onrender.com']
+ALLOWED_HOSTS = [
+    '.onrender.com',
+    'localhost',
+    '127.0.1',    
+    ]
 
 
 # Application definition
