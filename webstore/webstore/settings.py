190d100185ada17872d3de1a43e4c575fb740e5d
Kile-Butler01 <gucigovislam39@gmail.com>
2026-02-18 22:02:49 +0300

    Add support for images with integration with cloudinary

--- webstore/webstore/settings.py
+++ webstore/webstore/settings.py
@@ -14,8 +14,19 @@ import os
 from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
+import cloudinary
+
 
 load_dotenv()
+
+
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+)
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +42,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY=os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.onrender.com',
@@ -52,7 +63,9 @@ INSTALLED_APPS = [
     'users',
     'main',
     'rest_framework',
-    'products'
+    'products',
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
