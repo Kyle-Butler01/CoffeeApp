64e0ac6189d1871e179842f7e67554e7f67fc5c2
Kile-Butler01 <gucigovislam39@gmail.com>
2026-03-31 21:54:08 +0300

    Add redis cache on main pages

--- webstore/main/views.py
+++ webstore/main/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect
+from django.views.decorators.cache import cache_page
 
 
-
+@cache_page(60, key_prefix='home')
 def home_page (request):
     return render (request, 'main/index.html')
 
 
+@cache_page(60, key_prefix='contacts')
 def contacts_page (request):
     return render (request, 'main/contacts.html')
 
 
+@cache_page(60, key_prefix='cooperation')
 def cooperation_page (request):
     return render (request, 'main/cooperation.html')
 
